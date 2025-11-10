a2425f8d40659864f69a3bd1f80ff5a6b87417db
Poon Yip Hoon (Aeric) <yiphoon@yahoo.com>
2025-11-11 02:14:11 +0800

    resolve conflict

--- source/Objects/www/assets/js/main.js
+++ source/Objects/www/assets/js/main.js
@@ -1,35 +1,71 @@
 /*!
- * Pakai Server Framework B4J Project Template v5.50 by @pyhoon (https://github.com/pyhoon/pakai-server-b4j)
+ * Pakai Server Framework B4J Project Template v6.00 by @pyhoon (https://github.com/pyhoon/pakai-server-b4j)
  * Copyright (c) 2022-2025 Poon Yip Hoon (Aeric)
  * Licensed under MIT (https://github.com/pyhoon/pakai-server-b4j/blob/main/LICENSE)
  */
-var coll = document.getElementsByClassName("collapsible")
-
-var i
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active")
-    var details = this.nextElementSibling
-    if (details.style.maxHeight) {
-      details.style.maxHeight = null
-    } else {
-      details.style.maxHeight = details.scrollHeight + "px"
-    }
-  })
+// Toast functions
+function showToast(message, type = 'info') {
+    const toastContainer = document.getElementById('toast-container');
+    const toastId = 'toast-' + Date.now();
+    
+    const toastHTML = `
+        <div id="${toastId}" class="toast align-items-center text-bg-${type} border-0" role="alert">
+            <div class="d-flex">
+                <div class="toast-body">${message}</div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+            </div>
+        </div>
+    `;
+    
+    toastContainer.insertAdjacentHTML('beforeend', toastHTML);
+    const toast = new bootstrap.Toast(document.getElementById(toastId));
+    toast.show();
 }
 
-var csrf_token = $('meta[name="csrf-token"]').attr('content')
+function showSuccess(message) { showToast(message, 'success'); }
+function showWarning(message) { showToast(message, 'warning'); }
+function showDanger(message) { showToast(message, 'danger'); }
 
-function csrfSafeMethod(method) {
-  // these HTTP methods do not require CSRF protection
-  return (/^(GET|HEAD|OPTIONS)$/.test(method))
+// Modal management
+document.addEventListener('htmx:afterSwap', function(e) {
+    if (e.detail.target.id === 'modal-container') {
+        const modalElement = e.detail.target.querySelector('.modal');
+        if (modalElement) {
+            const bsModal = new bootstrap.Modal(modalElement);
+            
+            modalElement.addEventListener('hidden.bs.modal', function() {
+                document.getElementById('modal-container').innerHTML = '';
+            });
+            
+            bsModal.show();
+        }
+    }
+});
+
+// Modal functions - NO variable declarations that could conflict
+function closeCurrentModal() {
+    const modalElement = document.querySelector('.modal.show');
+    if (modalElement) {
+        const bsModal = bootstrap.Modal.getInstance(modalElement);
+        if (bsModal) {
+            bsModal.hide();
+        }
+    }
 }
 
-$.ajaxSetup({
-  beforeSend: function (xhr, settings) {
-    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-      xhr.setRequestHeader("x-csrf-token", csrf_token)
-    }
-  }
-})
+// Global error handler
+document.addEventListener('htmx:responseError', function (event) {
+  showError('Network error occurred. Please try again.');
+});
+
+// Fix bug for Chrome browser Blocked aria-hidden on an element
+// Source - https://stackoverflow.com/questions/79159883/warning-blocked-aria-hidden-on-an-element-because-its-descendant-retained-focu
+// Posted by Project Mayhem
+// Retrieved 11/5/2025, License - CC-BY-SA 4.0
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener('hide.bs.modal', function (event) {
+        if (document.activeElement) {
+            document.activeElement.blur();
+        }
+    });
+});
