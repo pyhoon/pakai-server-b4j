95a589f515bc0f91be4dbf0a33de344d87a2356c
Poon Yip Hoon (Aeric) <yiphoon@yahoo.com>
2026-07-06 21:58:20 +0800

    add swagger-ui assets

---
+++ source/Objects/www/swagger/swagger-initializer.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    //url: "https://petstore.swagger.io/v2/swagger.json",
+    // Tell Swagger to request the raw OpenAPI JSON specification from HelpHandler
+    url: "../help?format=openapi", 
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
